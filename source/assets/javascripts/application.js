f764d6935f0d1629642bb791f7859a081f24df34
Andreas Resios <andrei.resios@gmail.com>
2014-10-20 12:45:59 +0000

    Added graph visualization.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,29 +1,80 @@
-// require bootstrap
 //= require angular
 //= require angular-sanitize
+//= require gantt-chart-d3
 //
-//=  require_tree .
+//= require file-service
 //
 
 (function(){
-    var app = angular.module('SwfInvestigator',['SwfHelpers']);
+    var app = angular.module('SwfInvestigator',['SwfHelpers','Gantt']);
 
     app.controller('MainCtrl',[
-      '$scope', 'FileReader', 'HistoryTransformer'
-      ,function($scope, fileReader, historyTransformer){
+      '$scope', 'FileReader', 'HistoryTransformer', 'EventTransformer',
+      function($scope, fileReader, historyTransformer, eventTransformer){
         $scope.ctx = { inputHistory: '', history: [] };
+        var tasks =
+          [{"startDate":new Date("Sun Dec 09 01:36:45 EST 2012"),"endDate":new Date("Sun Dec 09 02:36:45 EST 2012"),"taskName":"E Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 04:56:32 EST 2012"),"endDate":new Date("Sun Dec 09 06:35:47 EST 2012"),"taskName":"A Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 06:29:53 EST 2012"),"endDate":new Date("Sun Dec 09 06:34:04 EST 2012"),"taskName":"D Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 05:35:21 EST 2012"),"endDate":new Date("Sun Dec 09 06:21:22 EST 2012"),"taskName":"P Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 05:00:06 EST 2012"),"endDate":new Date("Sun Dec 09 05:05:07 EST 2012"),"taskName":"D Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 03:46:59 EST 2012"),"endDate":new Date("Sun Dec 09 04:54:19 EST 2012"),"taskName":"P Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 04:02:45 EST 2012"),"endDate":new Date("Sun Dec 09 04:48:56 EST 2012"),"taskName":"N Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 03:27:35 EST 2012"),"endDate":new Date("Sun Dec 09 03:58:43 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 01:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 03:26:35 EST 2012"),"taskName":"A Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 03:00:03 EST 2012"),"endDate":new Date("Sun Dec 09 03:09:51 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 01:21:00 EST 2012"),"endDate":new Date("Sun Dec 09 02:51:42 EST 2012"),"taskName":"P Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 01:08:42 EST 2012"),"endDate":new Date("Sun Dec 09 01:33:42 EST 2012"),"taskName":"N Job","status":"FAILED"},
+            {"startDate":new Date("Sun Dec 09 00:27:15 EST 2012"),"endDate":new Date("Sun Dec 09 00:54:56 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 00:29:48 EST 2012"),"endDate":new Date("Sun Dec 09 00:44:50 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 07:39:21 EST 2012"),"endDate":new Date("Sun Dec 09 07:43:22 EST 2012"),"taskName":"P Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 07:00:06 EST 2012"),"endDate":new Date("Sun Dec 09 07:05:07 EST 2012"),"taskName":"D Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 08:46:59 EST 2012"),"endDate":new Date("Sun Dec 09 09:54:19 EST 2012"),"taskName":"P Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 09:02:45 EST 2012"),"endDate":new Date("Sun Dec 09 09:48:56 EST 2012"),"taskName":"N Job","status":"RUNNING"},
+            {"startDate":new Date("Sun Dec 09 08:27:35 EST 2012"),"endDate":new Date("Sun Dec 09 08:58:43 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 08:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 08:46:35 EST 2012"),"taskName":"A Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 08:00:03 EST 2012"),"endDate":new Date("Sun Dec 09 08:09:51 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 10:21:00 EST 2012"),"endDate":new Date("Sun Dec 09 10:51:42 EST 2012"),"taskName":"P Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sun Dec 09 11:08:42 EST 2012"),"endDate":new Date("Sun Dec 09 11:33:42 EST 2012"),"taskName":"N Job","status":"FAILED"},
+            {"startDate":new Date("Sun Dec 09 12:27:15 EST 2012"),"endDate":new Date("Sun Dec 09 12:54:56 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
+            {"startDate":new Date("Sat Dec 08 23:12:24 EST 2012"),"endDate":new Date("Sun Dec 09 00:26:13 EST 2012"),"taskName":"A Job","status":"KILLED"}];
+        var taskNames = [ "D Job", "P Job", "E Job", "A Job", "N Job", "default" ];
+        $scope.tasks = {tasks:[], taskTypes: [], taskNames: []};
         $scope.loadInput = function(files){
 
             fileReader.readAsText(files[0], $scope).then(function(data){
                 $scope.ctx.inputHistory = data;
                 // parse and show history
                 $scope.ctx.history = historyTransformer.fromJson(data);
+              $scope.tasks = eventTransformer.fromHistory($scope.ctx.history);
             })
         }
         $scope.updateDetails = function(index){
-          $scope.currentEvent = $scope.ctx.history[index];
+          if(index == $scope.ctx.currentIndex){
+            $scope.ctx.currentIndex = null;
+            $scope.ctx.currentEvent = null;
+          } else {
+            $scope.ctx.currentIndex = index;
+            $scope.ctx.currentEvent = $scope.ctx.history[index];
+          }
         }
-
-
     }]);
+
+  app.directive('swfTimestamp', function(){
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope: {timestamp: "=swfTimestamp"},
+      template: "<span class='timestamp' data-millis='{{timestamp}}'> {{timestamp | date:'yyyy-MM-ddTHH:mm:ssZ':'UTC'}}</span>"
+    }
+  });
+
+  app.directive('swfEvent', function(){
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope: {type: "=swfEvent"},
+      template: "<span class='swf-event swf-event-{{type|lowercase}}'> {{type}}</span>"
+    }
+  });
 })();
--- source/assets/javascripts/file-service.js
+++ source/assets/javascripts/file-service.js
@@ -80,6 +80,94 @@
       }
     }]);
 
+    helpers.factory('EventTransformer',[function(){
+      var EVENT_TYPE = {
+        'ActivityTaskScheduled': 'activity',
+        'DecisionTaskScheduled': 'decision',
+        'TimerStarted': 'timer'
+      };
+      var fromHistory = function(history){
+        // return an array of event objects that can be drawn on the screen
+        // events group multiple SWF events (at most three)
+        // they have start, end, type, parameters
+        var events = {};
+        if(angular.isArray(history)){
+          for(var e in history){
+            e = history[e];
+            var attributeName = e.eventType[0].toLowerCase() + e.eventType.slice(1) + "EventAttributes";
+            var attributes = e[attributeName] || {};
+            var event = null;
+            var eventDate = new Date(e.eventTimestamp);
+            if(/(scheduled|timerStarted)$/i.test(e.eventType)){
+              //event is scheduled add new event
+              events[e.eventId] = {startDate: eventDate, label: e.eventType, type: EVENT_TYPE[e.eventType]};
+            } else if(/started$/i.test(e.eventType)){
+              //event is started
+              event = events[attributes.scheduledEventId];
+              if(event){
+                event.endDate = eventDate;
+                //event.started = e;
+              }
+            } else if(/completed$/i.test(e.eventType)){
+              event = events[attributes.scheduledEventId];
+              if(event){
+                event.endDate = eventDate;
+                //event.completed = e;
+              }
+            } else if(/timer(fired|canceled)$/i.test(e.eventType)){
+              event = events[attributes.startedEventId];
+              if(event){
+                event.endDate = eventDate;
+              }
+            }
+          }
+        }
+
+        var result = [];
+        for(var k in events){
+          result.push(events[k]);
+        }
+
+        result.sort(function(a, b) {
+          return a.endDate - b.endDate;
+        });
+        result.sort(function(a, b) {
+          return a.startDate - b.startDate;
+        });
+        var lanes = [];
+        var taskNames = [];
+        for(var i in result){
+          var interval = result[i];
+          var assigned = false;
+          for(var l = 0; l < lanes.length; l++){
+            if(interval.startDate > lanes[l]){
+              assigned = true;
+              interval.taskName = 'L' + (l + 1);
+              lanes[l] = interval.endDate;
+              break;
+            }
+          }
+          if(!assigned){
+            lanes.push(interval.endDate);
+            interval.taskName = 'L' + lanes.length;
+            taskNames.push(interval.taskName);
+          }
+        }
+
+        return {tasks: result, taskNames: taskNames, taskTypes: taskNames};
+      }
+
+      return {
+        fromHistory: fromHistory
+      }
+    }]);
+
+    helpers.filter('dateFormatter', function(){
+      return function(obj){
+        return new Date(obj).toGMTString();
+      }
+    });
+
     helpers.filter('prettyPrint', ['$sce', function($sce){
       var transform = function(obj) {
         for (var key in obj) {
